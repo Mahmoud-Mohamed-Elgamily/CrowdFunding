9a096e9f6940585dccaff339cf6b38a492e48c71
mahmoedmohamed19@gmail.com <mahmoedmohamed19@gmail.com>
2020-03-18 21:05:41 +0200

    adding materialize

--- projects/views.py
+++ projects/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 
 def listProjects(req):
-    return HttpResponse("welcome")
+    # return HttpResponse("welcome")
+    return render(req , 'projects/projects.html')
