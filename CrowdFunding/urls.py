8c0fa804af70099c064873eb26392d61c5e8ffe3
mahmoedmohamed19@gmail.com <mahmoedmohamed19@gmail.com>
2020-03-20 00:33:41 +0200

    comit

--- CrowdFunding/urls.py
+++ CrowdFunding/urls.py
@@ -1,16 +1,14 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from projects import urls as ProjectUrls
-from userAuth import urls as userUrls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include("HomePage.urls", namespace="crowdFund")),
     path('', include("HomePage.urls")),
-    path('projects/', include(ProjectUrls)),
-    path('profile/', include(userUrls)),
+    path('projects/', include("projects.urls")),
+    path('profile/', include("userAuth.urls")),
     path('user/', include("user.urls")),
 
 ]
