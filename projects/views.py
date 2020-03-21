2931ffc7ecca725ca41924bff0f2c915b40cf8f4
mahmoedmohamed19@gmail.com <mahmoedmohamed19@gmail.com>
2020-03-21 21:15:49 +0200

    merge by mahmoud

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 from django.shortcuts import render, HttpResponse, redirect
-from .models import Projects, Donation, Tag
+from .models import Projects, Donation, Tag, Report, Comment
+from .form import CommentForm, ReportForm
 from django.urls import reverse
 from django.http import JsonResponse
 
@@ -8,12 +8,6 @@ import stripe
 
 stripe.api_key = 'sk_test_kQeGPyzUEtnwIWuGkSSnkEYy00MTZcMDFF'
 # publishkey 'pk_test_qldeOx2RsqvPXmVGAlXBlZQQ00qufvfDcj'
-=======
-from django.shortcuts import render, HttpResponse
-from .models import Projects , Report ,Comment
-from .form import CommentForm ,ReportForm
-# Create your views here.
->>>>>>> a0f7889ee8d577a71d3af4d897c43d3ea09211b9
 
 
 def listProjects(req):
@@ -63,47 +57,51 @@ def charge(req):
     # )
 
     return redirect(f"/projects/{project_id}", 'success')
-def selectProject(req,id):
-    project = Projects.objects.filter(id = int(id)).first()
+
+
+def selectProject(req, id):
+    project = Projects.objects.filter(id=int(id)).first()
     comments = Comment.objects.filter(project_id=project)
     comment = None
-  
+
     if project:
         if req.method == 'POST':
             comment_form = CommentForm(req.POST)
             if comment_form.is_valid():
-                comment_content=req.POST.get("comment_content")
-           
-                comment=Comment.objects.create(project_id=project,comment_content=comment_content)
+                comment_content = req.POST.get("comment_content")
+
+                comment = Comment.objects.create(
+                    project_id=project, comment_content=comment_content)
                 comment.save()
-              
+
         else:
             comment_form = CommentForm()
-            
-        return render(req,"projects/singleProject.html",{ "project" :project,
-        "comments":comments,
-        "comment_form":comment_form,
-       })
+
+        return render(req, "projects/singleProject.html", {"project": project,
+                                                           "comments": comments,
+                                                           "comment_form": comment_form,
+                                                           })
 
     else:
         return HttpResponse("404 Not Found")
 
-def reportProject(req ):
+
+def reportProject(req):
     project = Projects.objects.filter()
     reports = Report.objects.filter()
     if req.method == 'POST':
-            report_form = ReportForm(req.POST or None)
-            if report_form.is_valid():
-                report_content=req.POST.get("report_content")
-            
-                report=Report.objects.create(project_id=project,report_content=report_content)
-                report.save()
-                
+        report_form = ReportForm(req.POST or None)
+        if report_form.is_valid():
+            report_content = req.POST.get("report_content")
+
+            report = Report.objects.create(
+                project_id=project, report_content=report_content)
+            report.save()
+
     else:
         report_form = ReportForm()
-            
-    return render(req,"projects/report.html",{
-        "reports":reports,
-    "report_form":report_form,
-   })
 
+    return render(req, "projects/report.html", {
+        "reports": reports,
+        "report_form": report_form,
+    })
