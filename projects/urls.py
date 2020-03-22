491ae0219dabfcc3140b2010060bd6e3572cb8d8
mahmoedmohamed19@gmail.com <mahmoedmohamed19@gmail.com>
2020-03-22 13:59:15 +0200

    my final push !

--- projects/urls.py
+++ projects/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.listProjects , name= "projects" ),
     path('charge', views.charge , name= "charge" ),
     path('<project_id>', views.singleProject , name= "singleProject" ),
-    path('<int:id>',views.selectProject,name='selectProject'),
+    path('<project_id>/<success>', views.singleProject , name= "singleProject" ),
+    # path('<int:id>',views.singleProject,name='selectProject'),
     path('report/',views.reportProject,name='reportProject'),
     path('tag/<tag_name>', views.tagProjects , name= "tags" ),
 ]
--- projects/views.py
+++ projects/views.py
@@ -17,18 +17,46 @@ def listProjects(req):
     }
     return render(req, 'projects/projects.html', context)
 
-
-def singleProject(req, project_id):
-    current_project = Projects.objects.get(id=project_id)
+def singleProject(req, **kwargs):
+    print(kwargs)
+    this_project_id = kwargs['project_id']
+    current_project = Projects.objects.get(id=this_project_id)
     tags_id = [t.id for t in current_project.tag.all()]
+    comments = Comment.objects.filter(project_id=current_project)
+    comment = None
 
-    context = {
-        'project': current_project,
-        'activate': 'projects',
-        'similar': Projects.objects.filter(tag__in=tags_id).distinct()[:5],
-    }
+    if current_project:
+        if req.method == 'POST':
+            comment_form = CommentForm(req.POST)
+            if comment_form.is_valid():
+                comment_content = req.POST.get("comment_content")
 
-    return render(req, 'projects/singleProject.html', context)
+                comment = Comment.objects.create(
+                    project_id=current_project, comment_content=comment_content)
+                comment.save()
+
+        else:
+            comment_form = CommentForm()
+
+        current_donation = 20
+        # uncomment this and comment the above
+        # current_donation = sum(int(money) for money in [donation_value for donation_value in Donation.objects.filter(project_id=this_project_id)])
+
+        context = {
+            'project': current_project,
+            'activate': 'projects',
+            'similar': Projects.objects.filter(tag__in=tags_id).distinct()[:5],
+            "comments": comments,
+            "comment_form": comment_form,
+            "total_donation" : current_donation,
+            "donation_percent" : current_donation / current_project.total_donation * 100
+        }
+
+        if 'success' in kwargs:
+            context['success'] = True
+
+        return render(req, 'projects/singleProject.html', context)
+    return HttpResponse("404 Not Found")
 
 
 def tagProjects(req, tag_name):
@@ -36,54 +64,25 @@ def tagProjects(req, tag_name):
         'projects': Projects.objects.filter(tag__tag_name=tag_name),
         'tag': tag_name
     }
-    # print(context['project'])
     return render(req, 'projects/tagProjects.html', context)
 
 
 def charge(req):
-    print('Data:', req.POST)
 
-    # amount = int(req.POST['amount'])
+    amount = int(req.POST['amount'])
     project_id = req.POST['project_id']
-    # print(project_id)
     # customer = stripe.Customer.create(
     #     source=req.POST['stripeToken']
     # )
 
-    # donation = Donation.create(
-    #     project_id=customer,
-    #     user_id = current user ,
-    #     donation_amount=amount*100
-    # )
+    donation = Donation(
+        project_id= Projects.objects.get(id=project_id),
+        user_id=req.user, # add current user here if this didnt work
+        donation_amount=amount*100
+    )
+    donation.save()
 
-    return redirect(f"/projects/{project_id}", 'success')
-
-
-def selectProject(req, id):
-    project = Projects.objects.filter(id=int(id)).first()
-    comments = Comment.objects.filter(project_id=project)
-    comment = None
-
-    if project:
-        if req.method == 'POST':
-            comment_form = CommentForm(req.POST)
-            if comment_form.is_valid():
-                comment_content = req.POST.get("comment_content")
-
-                comment = Comment.objects.create(
-                    project_id=project, comment_content=comment_content)
-                comment.save()
-
-        else:
-            comment_form = CommentForm()
-
-        return render(req, "projects/singleProject.html", {"project": project,
-                                                           "comments": comments,
-                                                           "comment_form": comment_form,
-                                                           })
-
-    else:
-        return HttpResponse("404 Not Found")
+    return redirect(f"/projects/{project_id}/s")
 
 
 def reportProject(req):
